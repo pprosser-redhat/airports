7c50cd0c2ef20cf3af2a5b333a19cecab0930e24
Phil Prosser <pprosser@redhat.com>
2019-03-01 13:55:17 +0000

    tidied up and comments added to the processor

--- airports/src/main/java/org/mycompany/ReturnProcessor.java
+++ airports/src/main/java/org/mycompany/ReturnProcessor.java
@@ -15,9 +15,15 @@ public class ReturnProcessor implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		// TODO Auto-generated method stub
+		
+		
+		// Create JAXB list of operator types
+		
 		List<operators.OperatorType> operators = new ArrayList();
 		
+		
+		// Unmarshalling the response to JSON creates a list of map objects
+		// Need to iterate over the map, and create an operate type
 		@SuppressWarnings("unchecked")
 		List<Map> input = (List<Map>) exchange.getMessage().getBody(List.class);
 		
@@ -27,8 +33,14 @@ public class ReturnProcessor implements Processor {
 			op.setFlightOperatorType((String) operator.get("flight_operator_type"));
 			op.setUpdateDate(((Long) operator.get("update_date")).toString());
 			
+			// SOAP service expecting a list (governed by the unbounded sequence in the schema) therefore
+			// adding the operatortype to the operators list created above
+			
 			operators.add(op);
 		}
+		
+		// The root level of the schema has a sequence (governing the need for a list), therefore the 
+		// arraylist of operator types needs to be added to the root level sequence array list
 		
 		List response = new ArrayList();
 		response.add(operators);
